f64b5f1ee0e4f3e21b61020bf98239401c52d1f5
UNCROOS <any492266@gmail.com>
2024-04-14 23:24:09 +0900

    day1

---
+++ src/components/AtrcleList.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Atrcle from './Article.svelte';
+  import AtrcleEditForm from './AtrcleEditForm.svelte';
+    import AtrcleLoading from './AtrcleLoading.svelte';
+</script>
+
+<!-- slog-list-wrap start-->
+<div class="slog-list-wrap">    
+    <ul class="slog-ul">
+        <li class="mb-5">
+            <Atricle />
+        </li>
+    </ul>
+</div><!-- slog-list-wrap end-->
